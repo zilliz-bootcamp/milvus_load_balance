8163302cb94078485c7879e45ead6a728a974644
jingkl <34296482+jingkl@users.noreply.github.com>
2021-02-19 16:31:36 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
-#!/usr/bin/python3
-
 import threading
 import time
 from milvus import Milvus, IndexType, MetricType, Status
-
-exitFlag = 0
 
 class myThread (threading.Thread):
     def __init__(self, threadID, name, counter):
@@ -13,29 +9,49 @@ class myThread (threading.Thread):
         self.name = name
         self.counter = counter
     def run(self):
-        print ("开始线程：" + self.name)
-        milvus = Milvus(host='localhost', port='19585')
-        print_time(self.name, self.counter, 7)
-        print ("退出线程：" + self.name)
+        print ("开启线程： " + self.name)
+        # 获取锁，用于线程同步
+        threadLock.acquire()
+        print_time(self.name, self.counter, 3)
+        milvus = Milvus(host='localhost', port='19544')
+        # 释放锁，开启下一个线程
+        threadLock.release()
 
 def print_time(threadName, delay, counter):
     while counter:
-        if exitFlag:
-            threadName.exit()
-       # time.sleep(delay)
+        time.sleep(delay)
         print ("%s: %s" % (threadName, time.ctime(time.time())))
         counter -= 1
+
+threadLock = threading.Lock()
+threads = []
 
 # 创建新线程
 thread1 = myThread(1, "Thread-1", 1)
 thread2 = myThread(2, "Thread-2", 2)
-#thread3 = myThread(3, "Thread-3", 3)
+thread3 = myThread(3, "Thread-3", 2)
+thread4 = myThread(4, "Thread-4", 2)
+thread5 = myThread(5, "Thread-5", 2)
+
+
 
 # 开启新线程
 thread1.start()
 thread2.start()
-#thread3.start()
-thread1.join()
-thread2.join()
-#thread3.join()
+thread3.start()
+thread4.start()
+thread5.start()
+
+# 添加线程到线程列表
+threads.append(thread1)
+threads.append(thread2)
+threads.append(thread3)
+threads.append(thread4)
+threads.append(thread5)
+
+
+# 等待所有线程完成
+for t in threads:
+    t.join()
 print ("退出主线程")
+
